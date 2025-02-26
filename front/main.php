6500ad6781073484058c0cd674c90329b573a474
maplesift <a23050716@gmail.com>
2025-02-26 22:23:35 +0800

    02完成07

--- front/main.php
+++ front/main.php
@@ -1,1 +1,42 @@
-<h2>main</h2>
+<style>
+.type{
+    display: inline-block;
+    margin: -5px;
+}
+.fl1{
+    width: 20%;
+    display: inline-block;
+    vertical-align: top;
+}
+.fl2{
+    width: 50%;
+    display: inline-block;
+}
+.text{
+    display: none;
+}
+.title{
+    color: blue;
+}
+</style>
+<?php
+if(isset($_GET['type'])){
+
+    $row=$News->find(['type_id'=>$_GET['type']]);
+}
+    // dd($rows);
+?>
+<div>
+    <!-- 目前位置: 首頁 > 分類網誌 > <span ><?=$rows[0]['type'];?></span> -->
+</div>
+
+    <a href="?type=1" class="type"><button>健康新知</button></a>
+    <a href="?type=2" class="type"><button>菸害防制</button></a>
+    <a href="?type=3" class="type"><button>癌症防治</button></a>
+    <a href="?type=4" class="type"><button>慢性病防治</button></a>
+ <?php   if(isset($_GET['type'])):?>
+    <h2><?=$row['type'];?></h2>
+    <div>
+        <?=nl2br($row['text']);?>
+    </div>
+  <?php  endif;?>
--- front/po.php
+++ front/po.php
@@ -12,12 +12,19 @@
     width: 50%;
     display: inline-block;
 }
+.text{
+    display: none;
+}
+.title{
+    color: blue;
+}
 </style>
 <?php
-    $News->all(['type'=>$_GET['type']]);
+    $rows=$News->all(['type_id'=>$_GET['type']]);
+    // dd($rows);
 ?>
 <div>
-    目前位置: 首頁 > 分類網誌 > <span class="title"></span>
+    目前位置: 首頁 > 分類網誌 > <span ><?=$rows[0]['type'];?></span>
 </div>
 <fieldset class="fl1">
     <legend>分類網誌</legend>
@@ -28,4 +35,17 @@
 </fieldset>
 <fieldset class="fl2">
     <legend>文章列表</legend>
+    <?php foreach ($rows as $row) :
+     ?>
+     <div class="r-title">
+         <div class="title"><?=$row['title'];?></div>
+         <div class="text"><?=$row['text'] ;?></div>
+    </div>
+    <!-- <div class="text"><?=mb_substr($row['text'],0,25) ;?></div> -->
+    <?php endforeach ;?>
 </fieldset>
+<script>
+    $(".r-title").on("click",function(){
+        $(this).children('.title,.text').toggle();
+    })
+</script>
