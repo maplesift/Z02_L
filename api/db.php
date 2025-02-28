4608b340e0efef2bfac41f006b317f1d8e0f828f
maplesift <a23050716@gmail.com>
2025-02-28 23:32:55 +0800

    02l3很怪

--- api/db.php
+++ api/db.php
@@ -3,7 +3,7 @@ session_start();
 class DB{
     protected $table;
     protected $pdo;
-    protected $dsn="mysql:host=localhost;charset=utf8;dbname=db03_z02l2;";
+    protected $dsn="mysql:host=localhost;charset=utf8;dbname=db03_z02l3;";
     
     public function __construct($table) {
         $this->table = $table;
@@ -38,39 +38,39 @@ class DB{
     }
     function find($id){
         $sql = "SELECT * FROM $this->table";
-        if (is_array($id)) {
-            $where = $this->a2s($id);
-            $sql .= " where ".join(" && ",$where);       
+        if(is_array($id)){
+            $where =$this->a2s($id);
+            $sql .= " where ".join(" && ",$where);
         }else{
             $sql .= " where `id`='$id' ";
         }
         return $this->fetchOne($sql);
 
-    } 
-
+    }
     function del($id){
         $sql = "DELETE FROM $this->table";
-        if (is_array($id)) {
-            $where = $this->a2s($id);
-            $sql .= " where ".join(" && ",$where);       
+        if(is_array($id)){
+            $where =$this->a2s($id);
+            $sql .= " where ".join(" && ",$where);
         }else{
             $sql .= " where `id`='$id' ";
         }
         return $this->pdo->exec($sql);
+
     }
     function save($array){
         // update table set
         if(isset($array['id'])){
             $id=$array['id'];
             unset($array['id']);
-            $where=$this->a2s($array);
-            $sql = " UPDATE $this->table set ".join(',',$where)." where `id`='$id'";
+            $set= $this->a2s($array);
+            $sql = " UPDATE $this->table SET ".join(",",$set)." where `id`='$id' ";
+
         }else{
             // insert into table() values()
             $keys= array_keys($array);
-            $sql = " INSERT INTO $this->table(`".join("`,`",$keys)."`) values('".join("','",$array)."') ";
+            $sql = " INSERT INTO $this->table(`".join("`,`",$keys)."`) values('".join("','",$array)."')";
         }
-        return $this->pdo->exec($sql);
     }
     function count(...$array){
         $sql = "SELECT count(*) FROM $this->table";
@@ -86,49 +86,59 @@ class DB{
         }
         return $this->pdo->query($sql)->fetchColumn();
     }
-    
-
 }
 // db外
 function q($sql){
-    $dsn="mysql:host=localhost;charset=utf8;dbname=db03_z02l2;";
+    $dsn="mysql:host=localhost;charset=utf8;dbname=db03_z02l3;";
     $pdo=new pdo($dsn,'root','');
     return $pdo->query($sql)->fetchAll();
 }
 function qCol($sql){
-    $dsn="mysql:host=localhost;charset=utf8;dbname=db03_z02l2;";
+    $dsn="mysql:host=localhost;charset=utf8;dbname=db03_z02l3;";
     $pdo=new pdo($dsn,'root','');
     return $pdo->query($sql)->fetchColumn();
 }
 function dd($array){
     echo "<pre>";
-    print_r(($array));
+    print_r($array);
     echo "</pre>";
 }
 function to($url){
     header("location:".$url);
 }
 
+$Log=new DB("log");
+$User=new DB("user");
 $Total=new DB("total");
 $Que=new DB("que");
-$User=new DB("user");
 $News=new DB("news");
-$Log=new DB("log");
 
-if(!isset($_SESSION['total'])){
-    $today= date("Y-m-d");
-    $total=$Total->find(['date'=>$today]);
-    // 如果total有值
-    if($total){
-        // 更新今日瀏覽數
-        $total['total']++;
-        $Total->save($total);
+
+if(isset($_SESSION['total'])){
+    $today=date("Y-m-d");
+    $chk=$Total->find(['date'=>$today]);
+    if($chk){
+        $chk['total']++;
+        $Total->save($chk);
     }else{
-        // 如果total回傳沒有值
-         // 新增當天的紀錄
         $Total->save(['date'=>$today,'total'=>1]);
     }
-
     $_SESSION['total']=1;
-} 
+}
 
+// if(!isset($_SESSION['total'])){
+//     $today= date("Y-m-d");
+//     $total=$Total->find(['date'=>$today]);
+//     // 如果total有值
+//     if($total){
+//         // 更新今日瀏覽數
+//         $total['total']++;
+//         $Total->save($total);
+//     }else{
+//         // 如果total回傳沒有值
+//          // 新增當天的紀錄
+//         $Total->save(['date'=>$today,'total'=>1]);
+//     }
+
+//     $_SESSION['total']=1;
+// } 
