6ea76c5c2c9c518f607b606b9b088244c60afba3
maplesift <a23050716@gmail.com>
2025-03-17 14:18:20 +0800

    02l5 3 7end

--- front/main.php
+++ front/main.php
@@ -1,1 +1,22 @@
-ma
+<style>
+    .btn{
+        margin: -2px;
+    }
+</style>
+
+<a href="?do=main&type=1"><button class="btn">健康新知</button></a>
+<a href="?do=main&type=2"><button class="btn">菸害防制</button></a>
+<a href="?do=main&type=3"><button class="btn">癌症防治</button></a>
+<a href="?do=main&type=4"><button class="btn">慢性病防治</button></a>
+
+<?php 
+if(isset($_GET['type'])):
+    $row=$News->find(['type'=>$_GET['type']]);
+?>
+<h2><?=$row['type_name'];?></h2>
+<hr>
+<div>
+    <?=$row['title'];?>
+    <?=nl2br($row['text']);?>
+</div>
+<?php endif;?>
--- front/po.php
+++ front/po.php
@@ -1,1 +1,60 @@
-po
+<style>
+    .btn{
+        margin: -2px;
+    }
+    .fl1{
+        width: 20%;
+        display: inline-block;
+        vertical-align: top;
+    }
+    .fl2{
+        width: 70%;
+        display: inline-block;
+    }
+    .type{
+        display:block;
+    }
+    .detail{
+        display: none;
+    }
+    .title{
+        color: blue;
+    }
+</style>
+<?php 
+
+    $rows=$News->all(['type'=>$_GET['type']]);
+?>
+
+<div>
+    目前位置: 首頁 > 分類網誌 > <?=$rows[0]['type_name'];?>
+
+</div>
+<fieldset class="fl1">
+<legend>分類網誌</legend>
+    <a href="?do=po&type=1" class="type">健康新知</a>
+    <a href="?do=po&type=2" class="type">菸害防制</a>
+    <a href="?do=po&type=3" class="type">癌症防治</a>
+    <a href="?do=po&type=4" class="type">慢性病防治</a>
+</fieldset>
+<fieldset class="fl2">
+<legend>文章列表</legend>
+
+<?php foreach ($rows as $row):?>
+    <div class="r-title">
+        <div class="title">
+            <?=$row['title'];?>
+        </div>
+        <div class="detail">
+            <?=nl2br($row['text']);?>
+        </div>
+    </div>
+    <?php endforeach; ?>
+
+</fieldset>
+
+<script>
+    $(".r-title").on("click",function(){
+        $(this).children(".title,.detail").toggle()
+    })
+</script>
